b02284ee04c9b62f14a312424eb898bc69480f5e
Long <longkkbb@gmail.com>
2023-12-17 23:28:47 +0700

    Them cac file

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link href="https://fonts.cdnfonts.com/css/nova-square" rel="stylesheet">
+                
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<style>
+    .title{
+        font-size: 20px;
+        width: 100%;
+        font-family: 'Nova Square', sans-serif;
+        
+    }
+    .card{
+        margin-left: 12px;
+    }
+
+
+</style>
+
+<body>
+    <div class="header">
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+            <a class="navbar-brand">Drink Store</a>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>   
+            </form>
+
+            <?php
+                error_reporting(0);
+                $checked=$_POST['signed'];
+                if(!$checked){
+                   
+                    echo "
+                    <div class='signin'>
+                    <a href='signin.php' class='btn btn-outline-success btnDangNhap' >Đăng nhập</a>
+
+                </div>
+                    ";
+                    
+                }else{
+                    echo "
+                    <div class='signup'>
+                    <a href='index.php' class='btn btn-outline-success btnDangNhap' >Đăng xuất</a>
+
+                </div>
+                    ";
+                    
+                }
+            ?>
+                    
+            
+            </div>
+        </nav>
+
+    </div>
+
+
+
+    <div class="content row">
+        <div class="sidebar col-2 sidebar-edit">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                
+            </ul>
+        </div>
+
+        <div class="product-sale col">
+            <div class="title">
+            <span>SALE 12/12</span> 
+            </div>
+
+            <div class="product row">
+                <?php
+                    include "config.php";
+                    $sql = "select * from drink ";
+                    $stm = $pdh->query($sql);
+                    $rows3 = $stm->fetchAll(PDO::FETCH_NUM);
+                    foreach($rows3 as $row)
+                    {
+                        echo "
+                        <div class='card col-3' style='width: 18rem;'>
+                            <img src='./img_product/$row[4]' class='card-img-top' alt='...'>
+                                <div class='card-body'>
+                                    <h5 class='card-title'>$row[1]</h5>
+                                    <p class='card-text'>Giá bán $row[2]$</p>
+                                    <a href='#' class='btn btn-primary'>Thêm vào giỏ hàng</a>
+                                </div>
+                        </div>
+                        ";  
+                    }
+
+                ?>
+            </div>
+        </div>
+        <div class="product-bestseller"></div>
+        <div class="product-1"></div>
+        <div class="product-2"></div>
+
+    </div>
+
+    
+
+
+    
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="scripts.js"></script>
+</html>
---
+++ signup.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<script>
+    // Function để disable reload page
+    function disableReload() {
+        return false;
+    }
+</script>
+<body>
+<section class="vh-100" style="background-color: #eee;">
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style="border-radius: 25px;">
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+
+                <form class="mx-1 mx-md-4" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" onsubmit="return disableReload()">
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input name="name" type="text" id="form3Example1c" class="form-control" required  />
+                      <label class="form-label" for="form3Example1c">Your Name</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-phone fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                        <input type="tel" id="phone" name="phone" required class="form-control" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
+                        <label class="form-label" for="phone">Your Phone</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input name="password" type="password" id="form3Example4c" class="form-control" required/>
+                      <label class="form-label" for="form3Example4c">Password</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="password" id="form3Example4cd" class="form-control" required />
+                      <label class="form-label" for="form3Example4cd">Repeat your password</label>
+                    </div>
+                  </div>
+
+                  <div class="form-check d-flex justify-content-center mb-5">
+                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" required />
+                    <label class="form-check-label" for="form2Example3">
+                      I agree all statements in <a href="#!">Terms of service</a>
+                    </label>
+                  </div>
+
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <input  type="submit" class="btn btn-primary btn-lg" value="Register" />
+                  </div>
+
+                </form>
+
+              </div>
+              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                <img src="./img/anh1.jpg"
+                  class="img-fluid" alt="Sample image">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php
+    include "config.php";
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST["name"];
+    $pass = $_POST["password"];
+
+    $sql = "select * from user where user_name='$user' ";
+    $stm = $pdh->query($sql);
+    $rows = $stm->fetchAll(PDO::FETCH_NUM);
+    echo sizeof($rows);
+    if(sizeof($rows)){
+      echo "Tai khoan da ton tai";
+    }else{
+      header("Location: index.php");
+      exit();
+    }
+                    
+}
+?>
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</html>
